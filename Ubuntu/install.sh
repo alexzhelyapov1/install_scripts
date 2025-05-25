da2421116f2d9ffe9ac76cbd1d141b0cfcb9ef16
Zhelyapov Aleksey <alexzhelyapov1@mail.ru>
2025-05-25 13:17:06 +0300

    Added check for ubuntu

--- Ubuntu/install.sh
+++ Ubuntu/install.sh
@@ -9,6 +9,11 @@ if [ ! -f "$APT_REQUIREMENTS" ] || [ ! -f "$PIP_REQUIREMENTS" ]; then
     exit 1
 fi
 
+if ! command -v lsb_release >/dev/null || [ "$(lsb_release -is)" != "Ubuntu" ]; then
+    echo "[ERROR] This script for Ubuntu OS only."
+    exit 1
+fi
+
 if [ "$(id -u)" -ne 0 ]; then
     echo "This script must be run as root: sudo $0"
     exit 1
